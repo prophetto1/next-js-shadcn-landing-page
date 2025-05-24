33ab04056736e0019090f9c3c329c9ee4abd7bc2
prophetto1 <jonbiz717@gmail.com>
2025-05-24 06:12:39 -0700

    Update Features.tsx

--- components/Features.tsx
+++ components/Features.tsx
@@ -36,7 +36,7 @@ const features = [
 
 export const Features = () => {
   return (
-    <section className="container py-24 sm:py-32 space-y-8">
+<section id="features" className="container py-24 sm:py-32 space-y-8 scroll-mt-20">
       <h2 className="text-3xl lg:text-4xl font-bold md:text-center">
         AI-Powered{" "}
         <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
