2c8270617e31a7e83d51f3bb4d931ad067850693
prophetto1 <jonbiz717@gmail.com>
2025-05-24 05:40:30 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { About } from "@/components/About";
+import { About } from "@/components/layout/sections/about";
 import { Cta } from "@/components/Cta";
 import { FAQ } from "@/components/FAQ";
 import { Features } from "@/components/Features";
