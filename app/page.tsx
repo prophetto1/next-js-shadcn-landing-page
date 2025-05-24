77d9eae11f8cc2547e0669748cb81f03ffe6c5c4
prophetto1 <jonbiz717@gmail.com>
2025-05-24 05:49:56 -0700

    Try option 1 import paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,16 @@
-import { About } from "@/components/layout/sections/about";
-import { Cta } from "@/components/layout/sections/cta";
-import { FAQ } from "@/components/layout/sections/faq";
-import { Features } from "@/components/layout/sections/features";
-import { Hero } from "@/components/layout/sections/hero";
-import { HowItWorks } from "@/components/layout/sections/how-it-works";
-import { Newsletter } from "@/components/layout/sections/newsletter";
-import { Pricing } from "@/components/layout/sections/pricing";
-import { Services } from "@/components/layout/sections/services";
+import { About } from "@/components/about";
+import { Hero } from "@/components/hero";
+import { Features } from "@/components/features";
+import { Pricing } from "@/components/pricing";
+import { FAQ } from "@/components/faq";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
-      <HowItWorks />
       <Features />
-      <Services />
-      <Cta />
       <Pricing />
-      <Newsletter />
       <FAQ />
     </>
   );
